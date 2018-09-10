2305df7bd03071cfd2f10da8c110e198a6488f0c
ViXette <v.xette@gmail.com>
2018-09-10 20:12:07 +1000

    Initial commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by ViXette on 10/09/2018.
+//  Copyright © 2018 ViXette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
